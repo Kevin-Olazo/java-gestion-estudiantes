97d00a83e19545487e33fee14734bf711f638780
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-31 16:44:42 -0500

    Add constructor and methods to Persona class

--- src/main/java/com/gadev/Persona.java
+++ src/main/java/com/gadev/Persona.java
@@ -3,4 +3,28 @@ package com.gadev;
 public abstract class Persona {
     protected String nombre;
     protected String dni;
+
+    public Persona(String nombre, String dni) {
+        this.nombre = nombre;
+        this.dni = dni;
+    }
+
+    abstract String getTipoPersona();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", dni='" + dni + '\'' +
+                getTipoPersona() +
+                '}';
+    }
 }
