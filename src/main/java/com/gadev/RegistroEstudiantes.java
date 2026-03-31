68d30b9e29f48eae84fa8f78df87f48f3c37fad7
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-31 17:08:51 -0500

    feat: add validation and improve methods in RegistroEstudiantes class

--- src/main/java/com/gadev/RegistroEstudiantes.java
+++ src/main/java/com/gadev/RegistroEstudiantes.java
@@ -6,30 +6,53 @@ import java.util.Optional;
 public class RegistroEstudiantes {
     List<Estudiante> estudiantes;
 
-    public void registrarEstudiante(Estudiante estudiante){
-        if (estudiante == null){
-            throw new IllegalArgumentException("Estudiante no puede ser nulo");
+    public void registrarEstudiante(Estudiante estudiante) {
+        if (estudiante == null) {
+            throw new IllegalArgumentException("Estudiante no puede ser null");
         }
         estudiantes.add(estudiante);
     }
 
-    public void eliminarEstudiante(String dni){
+    public void eliminarEstudiante(String dni) {
+        // Validar el DNI
+        validarString(dni, "Dni");
 
+        estudiantes.removeIf(estudiante -> dni.equals(estudiante.getDni()));
     }
 
-    public Optional<Estudiante> buscarPorDni(String dni){
+    public Optional<Estudiante> buscarPorDni(String dni) {
+        // Validar el DNI
+        validarString(dni, "Dni");
+
+        // Buscar el estudiante por DNI
+        for (Estudiante estudiante : estudiantes) {
+            if (dni.equals(estudiante.getDni())) {
+                return Optional.of(estudiante);
+            }
+        }
+
+
         return Optional.empty();
     }
 
-    public List<Estudiante> buscarPorNombre(String nombre){
+    public List<Estudiante> buscarPorNombre(String nombre) {
+        // Validar el nombre
+        validarString(nombre, "Nombre");
         return null;
     }
 
-     public List<Estudiante> listarPorPromedio(){
+    public List<Estudiante> listarPorPromedio() {
         return null;
-     }
+    }
 
-     public void estadisticas(){
+    public void estadisticas() {
 
-     }
+    }
+
+    // Metodo auxiliar para validar String como DNI o Nombre
+    private void validarString(String valor, String nombreCampo) {
+        if (valor == null || valor.isBlank()) {
+            throw new IllegalArgumentException(nombreCampo + " no puede ser null o vacío");
+        }
+    }
 }
