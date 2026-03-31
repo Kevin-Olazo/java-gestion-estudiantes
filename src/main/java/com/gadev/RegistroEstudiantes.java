441f2d40a90a799d1f57ff429280cfc41f349a2a
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-31 16:59:06 -0500

    feat: implement student registration and search methods in RegistroEstudiantes class

--- src/main/java/com/gadev/RegistroEstudiantes.java
+++ src/main/java/com/gadev/RegistroEstudiantes.java
@@ -1,4 +1,35 @@
 package com.gadev;
 
+import java.util.List;
+import java.util.Optional;
+
 public class RegistroEstudiantes {
+    List<Estudiante> estudiantes;
+
+    public void registrarEstudiante(Estudiante estudiante){
+        if (estudiante == null){
+            throw new IllegalArgumentException("Estudiante no puede ser nulo");
+        }
+        estudiantes.add(estudiante);
+    }
+
+    public void eliminarEstudiante(String dni){
+
+    }
+
+    public Optional<Estudiante> buscarPorDni(String dni){
+        return Optional.empty();
+    }
+
+    public List<Estudiante> buscarPorNombre(String nombre){
+        return null;
+    }
+
+     public List<Estudiante> listarPorPromedio(){
+        return null;
+     }
+
+     public void estadisticas(){
+
+     }
 }
