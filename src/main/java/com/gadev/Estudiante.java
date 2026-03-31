dc076e790d92d77d1a9629862d5f83b9d122d165
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-31 16:45:53 -0500

    feat: add Estudiante class with properties and methods for managing grades

--- src/main/java/com/gadev/Estudiante.java
+++ src/main/java/com/gadev/Estudiante.java
@@ -1,4 +1,28 @@
 package com.gadev;
 
-public class Estudiante extends Persona {
+import java.util.List;
+
+public class Estudiante extends Persona implements Comparable<Estudiante> {
+    private String carrera;
+    private List<Double> notas;
+
+    public Estudiante(String nombre, String dni, String carrera) {
+        super(nombre, dni);
+        this.carrera = carrera;
+    }
+
+    private void agregarNota(double nota){
+        if (nota < 0 || nota > 20){
+            throw new IllegalArgumentException("Nota invalida (0-20)");
+        }
+        notas.add(nota);
+    }
+
+    private double getPromedio(){
+        // Calcular el promedio de las notas
+        return notas.stream()// Convertir la lista de notas a un stream
+                .mapToDouble(Double::doubleValue)// Convertir cada nota a un valor double
+                .average() // Calcular el promedio de las notas
+                .orElse(0.0); // Si no hay notas, retornar 0.0
+    }
 }
