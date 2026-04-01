db1aedcc8b04bb4ac22181d3bb53004e9f5d159e
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-31 20:23:19 -0500

    feat: refactor MenuRegistro methods for improved readability and functionality

--- src/main/java/com/gadev/MenuRegistro.java
+++ src/main/java/com/gadev/MenuRegistro.java
@@ -7,7 +7,7 @@ public class MenuRegistro {
     private final Scanner scanner = new Scanner(System.in);
     private RegistroEstudiantes registroEstudiantes = new RegistroEstudiantes();
 
-    public void start(){
+    public void start() {
 
         Estudiante e1 = new Estudiante("Juan Perez", "12345678", "Ingenieria de Sistemas");
         Estudiante e2 = new Estudiante("Maria Gomez", "87654321", "Administracion");
@@ -30,9 +30,11 @@ public class MenuRegistro {
             printMenu();
             System.out.print("Seleccione una opción: ");
             command = Integer.parseInt(scanner.nextLine());
-            switch (command){
+            switch (command) {
                 case 1 -> registrarNuevo();
-                case 3 -> buscarEstudiante().ifPresentOrElse(e -> System.out.println(e.toString()), () -> System.out.println("No se encontró  estudiante con ese dni"));
+                case 2 -> agregarNota();
+                case 3 ->
+                        buscarEstudiante().ifPresentOrElse(e -> System.out.println(e.toString()), () -> System.out.println("No se encontró  estudiante con ese dni"));
                 case 6 -> verEstadisticas();
                 case 8 -> System.out.println("Adios!");
                 default -> System.out.println("Ingrese un comando valido");
@@ -42,7 +44,7 @@ public class MenuRegistro {
 
     }
 
-    public void registrarNuevo(){
+    public void registrarNuevo() {
 
         System.out.println("Ingresa nombre: ");
         String nombre = scanner.nextLine();
@@ -51,28 +53,45 @@ public class MenuRegistro {
         System.out.println("Ingresa Carrera: ");
         String carrera = scanner.nextLine();
 
-        registroEstudiantes.registrarEstudiante(new Estudiante(nombre,dni, carrera));
+        registroEstudiantes.registrarEstudiante(new Estudiante(nombre, dni, carrera));
     }
 
-    public void agregarNota(double nota){
+    public void agregarNota() {
+        Optional<Estudiante> estudiante = buscarEstudiante();
 
-        buscarEstudiante().ifPresentOrElse(e -> e.agregarNota(nota), () -> System.out.println("No se encontró el estudiante"));
+        if (estudiante.isEmpty()) {
+            System.out.println("No se encontro el estudiante con ese DNI");
+            return;
+        }
+
+
+        try {
+            System.out.print("Ingrese la nota: ");
+            double nota = Integer.parseInt(scanner.nextLine());
+            estudiante.get().agregarNota(nota);
+            System.out.println("Nota agregada correctamente");
+        } catch (NumberFormatException e) {
+            System.out.println("Ingrese un valor valido.");
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+
 
     }
 
-    public Optional<Estudiante> buscarEstudiante(){
+    public Optional<Estudiante> buscarEstudiante() {
         System.out.print("Ingrese dni del estudiante: ");
         String dni = scanner.nextLine();
         return registroEstudiantes.buscarPorDni(dni);
     }
 
-    public void verEstadisticas(){
-        for(Estudiante e : registroEstudiantes.getLista()){
+    public void verEstadisticas() {
+        for (Estudiante e : registroEstudiantes.getLista()) {
             System.out.println(e.toString());
         }
     }
 
-    public void printMenu(){
+    public void printMenu() {
         System.out.println("------- Menu de registro de estudiantes --------");
         System.out.println("1. Registrar nuevo estudiante");
         System.out.println("2. Agregar nota a estudiante");
